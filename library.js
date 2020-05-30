88560823c642c940c663947861dba153d91bf8b4
Godzapon <56731094+Godzapon@users.noreply.github.com>
2020-05-30 15:23:52 +0200

    Seplace section tag by div
    Test to check compatibility with NodeBB 1.13.x

--- library.js
+++ library.js
@@ -27,7 +27,7 @@ const replaceSpoilerContent = (content) => {
 const formatSpoiler = (text) => {
 	const startSpoiler = new RegExp(/\[s(?:=([^\]]*)|)\]/g);
 	const endSpoiler = new RegExp(/\[\/s\](<\/li>\n<\/(?:ol|ul)>|)/g);
-	return text.replace(startSpoiler, (match, p1) => `<section class="spoiler-wrapper"><button class="spoiler-control btn btn-default">${p1 ? p1 : 'Spoiler'}</button><section style="display: none;" class="spoiler-content">`).replace(endSpoiler, (match, p1) => `${p1}</section></section>`);
+	return text.replace(startSpoiler, (match, p1) => `<div class="spoiler-wrapper"><button class="spoiler-control btn btn-default">${p1 ? p1 : 'Spoiler'}</button><div style="display: none;" class="spoiler-content">`).replace(endSpoiler, (match, p1) => `${p1}</div></div>`);
 }
 
 const parsePost = (data, callback) => {
